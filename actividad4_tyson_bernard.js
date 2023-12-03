c526bf658b3713a5fcffac117646539b0efe5d84
Tyson Bernard <tyson564@hotmail.com>
2023-12-03 16:07:19 -0500

    Inserción de registros en la colección 'deportistas'

--- actividad4_tyson_bernard.js
+++ actividad4_tyson_bernard.js
@@ -27,5 +27,44 @@
   db.createCollection('arbitros');
   db.createCollection('encuentros_deportivos');
   db.createCollection('resultados');
+
+  // Inserción de registros en la colección 'deportistas'
+  db.deportistas.insert([
+    {
+      "nombre": "John Doe",
+      "edad": 28,
+      "categoria_peso": "Mediano",
+      "equipo": "Team Alpha",
+      "record": { "victorias": 10, "derrotas": 3, "empates": 1 }
+    },
+    {
+      "nombre": "Mike Tyson",
+      "edad": 54,
+      "categoria_peso": "Pesado",
+      "equipo": "Iron Fist Gym",
+      "record": { "victorias": 50, "derrotas": 6, "empates": 2 }
+    },
+    {
+      "nombre": "Tyson Bernard",
+      "edad": 30,
+      "categoria_peso": "Wélter",
+      "equipo": "Tiger MMA",
+      "record": { "victorias": 12, "derrotas": 2, "empates": 0 }
+    },
+    {
+      "nombre": "Deontay Wilder",
+      "edad": 35,
+      "categoria_peso": "Pesado",
+      "equipo": "Wilder's Warriors",
+      "record": { "victorias": 42, "derrotas": 1, "empates": 1 }
+    },
+    {
+      "nombre": "Jon Jones",
+      "edad": 34,
+      "categoria_peso": "Semipesado",
+      "equipo": "Jackson-Wink MMA",
+      "record": { "victorias": 26, "derrotas": 1, "empates": 0 }
+    }
+  ]);
   
   print("Base de datos del torneo creada exitosamente.");
